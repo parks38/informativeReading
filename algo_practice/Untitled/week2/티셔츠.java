f668d721cb75d0d47637b13471c0223aa97dd6ed
Sunny Park <parks38@uw.edu>
2022-10-28 09:35:37 +0900

    Update 티셔츠.java

--- algo_practice/Untitled/week2/티셔츠.java
+++ algo_practice/Untitled/week2/티셔츠.java
@@ -1,69 +1,36 @@
-import java.util.HashMap;
-import java.util.Map;
-
-class Solution {
-
+class 티셔츠 {
     public static void main (String[] args) {
-        int[] people = {1,2,3,4,5};
-        int[] tshirt = {1,3,3,3};
-        System.out.println(solution(people, tshirt));
+        int[https://github.com/parks38/swe-skills/blob/main/algo_practice/Untitled/week2/%ED%8B%B0%EC%85%94%EC%B8%A0.java] p = {1, 2, 3, 4, 5};
+        int[] ts = {1, 1, 2, 4};
+
+        System.out.println(solution(p,ts));
     }
+
     public static int solution(int[] people, int[] tshirts) {
+        // people, tshirts 배열은 정렬되어 있다는 조건이 없다는 것에 주의하세요
+        Arrays.sort(people);
+        Arrays.sort(tshirts);
+
         int answer = 0;
 
-        Map<Integer, Integer> shirt = new HashMap<>();
-
-        for (int i = 0; i < tshirts.length; i++) {
-            shirt.put(tshirts[i], shirt.getOrDefault(tshirts[i], 0) + 1);
-        }
-
-        Map<Integer, Integer> pple = new HashMap<>();
-        for (int i = 0; i < people.length; i++) {
-            pple.put(people[i], pple.getOrDefault(people[i], 0) + 1);
-        }
-        int leftOverPPle = 0;
-        // 큰 티셔츠 받을수 있음.
         /**
-         *  shirt 기준으로
-         *  shirt_count == pple_count | leftOverPPle == 0; answer += pple_count;
-         *  shirt_count > pple_count | leftOverPPle == 0; answer += pple_count + Math.abs(shirt_count - pple_count-leftOverPple);
-         *                             leftOverPpple = Math.abs(leftOverPple - shirt_count - pple_count)
-         *  shirt_count < pple_count | leftOverPple += (pple_count - shirt_count); answer += Math.abs(shirt_count - pple_count);
+         * 참가자가 자신보다 큰 티셔츠를 선택 가능하기 때문에
+         * 큰 티셔츠 부터 나눠주는 것으로 설계
          */
-        for (Map.Entry<Integer, Integer> entry : shirt.entrySet()) {
-
-            int s = shirt.get(entry.getKey());
-            int p = pple.getOrDefault(entry.getKey(), 0);
-            System.out.println("$$$" + leftOverPPle);
-             if (s == p) {
-                 answer += p;
-             } else if (s > p) {
-                 int currLeft = leftOverPPle > s ? leftOverPPle - s : leftOverPPle;
-                 leftOverPPle -= currLeft;
-                 answer += p + currLeft;
-
-             } else {
-                 leftOverPPle += (p-s);
-                 answer += Math.abs(s - p);
-             }
-
+        int i = tshirts.length - 1;
+        for (int j = people.length - 1; j >= 0 && i >= 0; j--) {
+            /**
+             * 마지막에 작은 티셔츠 크기들만 남게 됨.
+             * ex.   4 * 2 1 1
+             *     5 4 3 2 1
+             *     * O * O O *
+             */
+            if (tshirts[i] >= people[j]) {
+                answer += 1;
+                i -= 1;
+            }
         }
 
         return answer;
     }
-
-    // runtimeError
-//        Map<Integer, Integer> shirt = new HashMap<>();
-//
-//        for (int i = 0; i < tshirts.length; i++) {
-//            shirt.put(tshirts[i], shirt.getOrDefault(tshirts[i], 0) + 1);
-//        }
-//
-//        for (int i = 0; i < people.length; i++) {
-//            int count = shirt.getOrDefault(people[i], 0); // size 찾기
-//            if (count > 0) {
-//                answer++;
-//                shirt.put(tshirts[i], count - 1);
-//            }
-//        }
 }
